25f9cfdd5b037408093ddd87e8daadceca54ac7f
Ajurgs <ajjurgs5954@gmail.com>
2021-06-01 12:10:40 -0600

    added roles model

---
+++ models/roles.js
@@ -0,0 +1,26 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Role extends Model {}
+
+Role.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    roleName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: false,
+    underscored: true,
+    timestamps: false,
+    modelName: "roles",
+  }
+);
