157dd7db7da97ca6bdc291e8637fb8549cf4e55c
Ajurgs <ajjurgs5954@gmail.com>
2021-06-02 11:43:34 -0600

    added login route to homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,6 +4,15 @@ const withAuth = require("../utils/auth");
 router.get("/account", withAuth, async (req, res) => {
   // send to user accounts
 });
+
+router.get("/login", (req, res) => {
+  try {
+    res.render("login");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get("/signup", (req, res) => {
   try {
     res.render("signup");
