0903907b2de198220be28e5923f7319c2fb20dfe
Ajurgs <ajjurgs5954@gmail.com>
2021-06-02 10:27:31 -0600

    added the session info to the create user route

--- controllers/api/user.js
+++ controllers/api/user.js
@@ -23,6 +23,15 @@ router.get("/:id", async (req, res) => {
       });
       return;
     }
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+// create user
+router.post("/", async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
     req.session.save(() => {
       req.session.userId = userData.id;
       req.session.role = userData.role;
@@ -33,15 +42,6 @@ router.get("/:id", async (req, res) => {
         message: "You are now Logged in",
       });
     });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-// create user
-router.post("/", async (req, res) => {
-  try {
-    const userData = await User.create(req.body);
-    res.status(200).json(userData);
   } catch (err) {
     res.status(400).json(err);
   }
