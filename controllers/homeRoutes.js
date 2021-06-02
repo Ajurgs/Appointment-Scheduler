b42994bc59e9d4d1aa28426afa6a57d3fa700118
Ajurgs <ajjurgs5954@gmail.com>
2021-06-02 11:31:47 -0600

    added signup route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,5 +3,11 @@ const router = require("express").Router();
 router.get("/account", withAuth, async (req, res) => {
   // send to user accounts
 });
-
+router.get("/signup", (req, res) => {
+  try {
+    res.render("signup");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
