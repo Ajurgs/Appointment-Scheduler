bc88e11466d841b3325bd598267b4fecad83e12b
Jon Gregory <jongregory75@hotmail.com>
2021-06-03 21:45:05 -0600

    public/js/customerAccount.js         public/js/customerCalendar.js public/js/customerCalendar.js

---
+++ public/js/customerCalendar.js
@@ -0,0 +1,29 @@
+
+const formattedData = [];
+async function getAppointments(){ 
+  const userId = document.querySelector('#userId');
+
+    const temp = await fetch(`/api/appointment/requester/${userId}`);
+    await temp.json().then(data =>{
+    data.map(event =>{
+      formattedData.push({id:event.id,title:"test Title",start:event.start,end:event.end})
+    })
+
+    var calendarEl =  document.getElementById('calendar'); 
+    var calendar = new FullCalendar.Calendar(calendarEl, { 
+      schedulerLicenseKey: 'GPL-My-Project-Is-Open-Source',
+      // plugins: [ 'interaction'], 
+      eventClick: function (info) {
+        console.log(info.event);
+      }, 
+      timeZone: 'local', 
+      initialView: 'listWeek',
+      events: formattedData
+    });
+    calendar.render();
+  });
+}
+getAppointments();
+
+
+  
