baf5d02fca1eef660ca12c15e7004b4dadac97ba
Ajurgs <ajjurgs5954@gmail.com>
2021-06-04 15:47:17 -0600

    made minor fixes and linked the signup button to the signup page

--- controllers/index.js
+++ controllers/index.js
@@ -3,11 +3,9 @@ const router = require("express").Router();
 
 const apiRoutes = require("./api");
 const homeRoutes = require("./homeRoutes");
-const managerRoutes = require("./managerRoutes");
 
 router.use("/api", apiRoutes);
 router.use("/", homeRoutes);
-router.use('/manager', managerRoutes);
 
 // Export session information
 module.exports = router;
